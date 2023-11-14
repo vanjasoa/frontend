e3f067a0c13ad6a600112a22543afdceb4fe7980
vanjasoa <vanjasoa@example.com>
2023-11-14 13:12:16 +0300

    perf: :art: optimize

--- components/TopNavigationBar.vue
+++ components/TopNavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-[208px] bg-[#E61B21] flex justify-around items-center">
+    <div class="w-full h-[420px] bg-[#E61B21] flex justify-around items-center">
         <slot name="logo" />
         <div class="flex justify-end">
             <slot name="profil" />
