996e4cba5191e624c836eff13e5175795029a824
vanjasoa <vanjasoa@example.com>
2023-11-14 09:25:37 +0300

    feat: :bug: init auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules:['nuxt-directus'],
+  directus: {
+    url: "https://directus-production-2a05.up.railway.app/"
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,42 @@
 <template>
-    <h1>hello</h1>
+    <p v-if="user" >
+        {{ user }}
+        <h1>
+            {{ product }}
+        </h1>
+        <button @click="fetchProducts">refresh data</button>
+    </p>
+    <h1>Login</h1>
+    <button @click="onSubmit(userData)">Login</button>
+    <h1>Logout</h1>
+    <button @click="logout">Logout</button>
 </template>
+
+<script setup>
+const { getItems } = useDirectusItems();
+const { login, logout } = useDirectusAuth();
+const user = useDirectusUser();
+const product = ref({})
+
+const userData = ref({email:'teddy@mail.com',password:'123456'})
+
+const onSubmit = async (logindata) => {
+    try {
+        await login({ email: logindata.email, password: logindata.password });
+        
+    } catch (e) {
+        console.log(e)
+     }
+};
+
+const fetchProducts = async () => {
+  try {
+    
+    const items = await getItems({
+      collection: "product"
+    });
+    product.value = items
+  } catch (e) {}
+};
+
+</script>
