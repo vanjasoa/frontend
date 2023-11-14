172e60d5efb0089c603121936a790a1168e7f3d4
vanjasoa <vanjasoa@example.com>
2023-11-14 09:29:54 +0300

    feat: :sparkles: init tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules:['nuxt-directus'],
+  modules:[
+    'nuxt-directus',
+  '@nuxtjs/tailwindcss'
+],
   directus: {
     url: "https://directus-production-2a05.up.railway.app/"
   }
