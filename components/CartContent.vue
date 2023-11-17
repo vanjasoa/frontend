cd4feecfd4f81a68e07665abb8e2daeba885c0c1
vanjasoa <vanjasoa@example.com>
2023-11-17 15:48:10 +0300

    feat: :construction: add panier

--- components/CartContent.vue
+++ components/CartContent.vue
@@ -1,13 +1,20 @@
 <template>
     <div v-for="[name, items] in Object.entries(productCart.groupedItems)">
         {{ name }}
-        <h2>{{ items.length }} X {{ items.map((i) => i.prix)[0] }} = {{ items.map((i) => i.prix).reduce((totalItemPrice,
-            prix) => totalItemPrice + prix, 0) }}</h2>
+        <h2>{{ items.length }} X {{ items.map((i) => i.prix)[0] }} Ariary = {{ items.map((i) => i.prix).reduce((totalItemPrice,
+            prix) => totalItemPrice + prix, 0) }} Ariary</h2>
+        <h4>{{ items.length }} X {{ items.map((i) => i.point)[0] }} Point = {{ items.map((i) => i.point).reduce((totalItemPrice,
+            point) => totalItemPrice + point, 0) }} Point</h4>
     </div>
     <div>
         Total {{
             productCart.items
                 .map((p) => p.prix)
+                .reduce((acc, curr) => acc + curr, 0)
+        }}
+        Total Point {{
+            productCart.items
+                .map((p) => p.point)
                 .reduce((acc, curr) => acc + curr, 0)
         }}
     </div>
