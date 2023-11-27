837332290bdceb638cc88f84a3241cc49fdb57e6
vanjasoa <vanjasoa@example.com>
2023-11-27 13:11:08 +0300

    feat: :bug: fix image modal rewards

--- components/LevelsCard.vue
+++ components/LevelsCard.vue
@@ -26,7 +26,9 @@
       </div>
     </div>
     <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-white" v-if="showRewards === true">
-            <slot name="category" />
+      <div class="flex p-4">
+        <slot name="category" />
+      </div>      
             <slot name="products" />
     </div>
   </template>
