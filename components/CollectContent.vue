1e5d19008811148580f4ed9e22ce183303515e78
vanjasoa <vanjasoa@example.com>
2023-11-27 14:12:21 +0300

    style: :art: format

--- components/CollectContent.vue
+++ components/CollectContent.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <div class="flex flex-col items-center">
-            <div class="mt-[-50px] z-10">
-                <slot name="retour" />
-            </div>
-            
-            <div class="flex justify-around my-2 w-full">
-                <slot name="category" />
-            </div>
-            <div class="flex justify-around my-2 w-full">
-                <slot name="pub" />
-            </div>
-            <div class="flex flex-col w-full">
-                <div class="grid grid-cols-2 gap-8 p-4">
-                    <slot name="productlist" />
-                </div>
-            </div>
-
+    <div class="flex flex-col items-center">
+        <div class="mt-[-50px] z-10">
+            <slot name="retour" />
         </div>
+
+        <div class="flex justify-around my-2 w-full">
+            <slot name="category" />
+        </div>
+        <div class="flex justify-around my-2 w-full">
+            <slot name="pub" />
+        </div>
+        <div class="flex flex-col w-full">
+            <div class="grid grid-cols-2 gap-8 p-4">
+                <slot name="productlist" />
+            </div>
+        </div>
+
     </div>
 </template>
--- components/RewardsContent.vue
+++ components/RewardsContent.vue
@@ -1,20 +1,18 @@
 <template>
-    <div>
-        <div class="flex flex-col justify-center items-center">
-            <div class="mt-[-50px] flex flex-col items-center">
-                <slot name="retour" />
-                <h2 class="font-extrabold text-xl m-4">
-                    Qu’est ce qui est <span class="text-red-600">mieux</span> qu'une pizza ?
-                    Une pizza <span class="text-red-600">gratuite</span>.
-                </h2>
-                <p class="m-4 text-gray-400">
-                    Plus vous collectez des points,
-                    plus vos récompenses seront grandes.
-                </p>
-            </div>
-            <div class="flex w-full flex-col p-4">
-                <slot name="levels" />
-            </div>
+    <div class="flex flex-col justify-center items-center">
+        <div class="mt-[-50px] flex flex-col items-center">
+            <slot name="retour" />
+            <h2 class="font-extrabold text-xl m-4">
+                Qu’est ce qui est <span class="text-red-600">mieux</span> qu'une pizza ?
+                Une pizza <span class="text-red-600">gratuite</span>.
+            </h2>
+            <p class="m-4 text-gray-400">
+                Plus vous collectez des points,
+                plus vos récompenses seront grandes.
+            </p>
+        </div>
+        <div class="flex w-full flex-col p-4">
+            <slot name="levels" />
         </div>
     </div>
 </template>
