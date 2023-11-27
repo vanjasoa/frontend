da8f6d852ee60fe73d0254501b048814d9080b37
vanjasoa <vanjasoa@example.com>
2023-11-27 12:18:21 +0300

    feat: :art: add illustration

--- components/CategoryCard.vue
+++ components/CategoryCard.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="flex w-full border-2 rounded-xl items-end mx-2">
-
-        <h1 class="font-extrabold text-xl items my-2 mx-auto">{{ props.name }}</h1>
-
+        <img class="rounded-md" :src="props.name+'.jpeg'" alt="">
     </div>
 </template>
 
