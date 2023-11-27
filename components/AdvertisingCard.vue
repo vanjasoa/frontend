87f4cfc7fefc6b31bb11dea7e65d61793737e223
vanjasoa <vanjasoa@example.com>
2023-11-27 12:22:54 +0300

    fix: :bug: fix image

--- components/AdvertisingCard.vue
+++ components/AdvertisingCard.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="border-2 w-full h-[140px] mx-2 rounded-md">
-        <img src="pub.jpeg" alt="">
+        <img :src="pub+'jpeg'" alt="">
     </div>
 </template>
+
+<script setup>
+const pub = ref('pub')
+</script>
