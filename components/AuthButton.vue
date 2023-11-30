edb77b1b785f4b6418338b85939408fdb72d71f7
Fenohasina RASOAVARISON <fenohasina17>
2023-11-30 13:14:10 +0300

    style: fix welcome stylesheet

--- components/AuthButton.vue
+++ components/AuthButton.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col items-center mx-16">
-        <div class="w-full h-[70px] border-4 border-black rounded-lg flex justify-around lg:w-64">
+    <div class="flex flex-col items-center -mt-8 mx-16 ">
+        <div class="w-full  bg-white h-[70px] border-4 border-black rounded-lg flex justify-around lg:w-64">
             <button @click="showLogin" :class="{ 'bg-black text-white p-2 m-2 transition-shadow delay-500 rounded-md': activeTab === 'login' }" class="font-extrabold " >CONNEXION</button>
             <button @click="showRegister" :class="{ 'bg-black text-white p-2 m-2 transition-shadow delay-500 rounded-md': activeTab === 'register' }" class="font-extrabold" >INSCRIPTION</button>
         </div>
--- components/MainNavigationBar.vue
+++ components/MainNavigationBar.vue
@@ -6,12 +6,21 @@
 
         <div class="flex justify-end items-center m-4">
             <slot name="profil" />
-        </div>v>-->
+        </div>
+    </div>     -->
     <img src="../assets/images/header.png" alt="">
     <div class="flex items-center justify-center">
-        <div class="w-40 h-18 -mt-48 object-cover">
+        <!-- <div class="w-40 h-18 -mt-48 object-cover">
             <slot name="logo" />
         </div>
-            
+             -->
+         <div  class="w-40 h-16 -mt-60 object-cover">
+            <slot name="logo" />
+        </div>
+
+        <div class="flex justify-end items-center  -mt-52">
+            <slot name="profil" />
+        </div>
     </div>
+    
 </template>
