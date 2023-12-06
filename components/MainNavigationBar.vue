d66b3d46e4c3bf7807bbca5aa85ace434453aeae
Fenohasina RASOAVARISON <fenohasina17>
2023-12-06 06:17:31 +0300

    fix: fix z-indexing on mainNavigation

--- components/MainNavigationBar.vue
+++ components/MainNavigationBar.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template >
     <div class="z-40">
         <img src="../assets/images/header.png" alt="">
@@ -10,30 +9,6 @@
             <div class="flex justify-end items-center  -mt-52">
                 <slot name="profil" />
             </div>
-=======
-<template>
-    <!-- <div class=" h-30 bg-[#E61B21] flex justify-around rounded-b-full items-center lg:justify-end lg:flex" style="width: 800px;">
-        <div class="mx-auto lg:-mx-200">
-            <slot name="logo" />
-        </div>
-
-        <div class="flex justify-end items-center m-4">
-            <slot name="profil" />
-        </div>
-    </div>     -->
-    <img src="../assets/images/header.png" alt="">
-    <div class="flex items-center justify-center">
-        <!-- <div class="w-40 h-18 -mt-48 object-cover">
-            <slot name="logo" />
-        </div>
-             -->
-         <div  class="w-40 h-16 -mt-60 object-cover">
-            <slot name="logo" />
-        </div>
-
-        <div class="flex justify-end items-center  -mt-52">
-            <slot name="profil" />
->>>>>>> 8d2f845ec086e4616c893b50d153cfaec25c8866
         </div>
     </div>
     
