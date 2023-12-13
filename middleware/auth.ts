4594cb0b4b560e329dce3ac55c1d606901081b7c
vanjasoa <vanjasoa@example.com>
2023-12-13 10:57:20 +0300

    fix: :bug: refresh welcome begin

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,10 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   const { fetchUser, setUser } = useDirectusAuth();
-  const user = useDirectusUser();
-  if (!user.value) {
-    const user = await fetchUser();
-    setUser(user.value);
-  }
+  //const user = useDirectusUser();
+  const user = await fetchUser();
+  //console.log('Cookies:', document.cookie);
+  // if (!user.value) {
+  //   const user = await fetchUser();
+  //   setUser(user.value);
+  // }
   if (!user.value) {
     return navigateTo("/");
   }
