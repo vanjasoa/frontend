9bb80daa4c527df35e3e8ab71a547d56603cf79f
vanjasoa <vanjablou@gmail.com>
2023-12-15 17:47:02 +0300

    Gestion register page (#6)
    * fead:Mise en place des validations des inputs du register
    
    * feat:validation register
    
    ---------
    
    Co-authored-by: tiana777 <tiana.benjamin.rakotomalala@gmail.com>

--- components/AuthCard.vue
+++ components/AuthCard.vue
@@ -6,9 +6,11 @@
             <p class=" text-gray-600 text-xs  my-2">
                 Connectez-vous avec votre nom d’utilisateur ou numéro de téléphone
             </p>
-            <input name="email" v-model="loginForm.email" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs my-4"
+            <input name="email" v-model="loginForm.email"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs my-4"
                 type="text" placeholder="Votre nom d’utilisateur ou téléphone">
-            <input name="password" v-model="loginForm.password"  class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs my-2"
+            <input name="password" v-model="loginForm.password"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs my-2"
                 type="password" placeholder="Votre mot de passe">
             <div class="flex justify-between w-full my-4">
                 <div>
@@ -27,71 +29,83 @@
             <p class="text-xs mx-auto my-1 text-gray-600">
                 Remplissez les champs ci-dessous pour vous inscrire.
             </p>
-
-            <input v-model="registerForm.email" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-6"
+            <input v-model="registerForm.email"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-6"
                 type="text" placeholder="Votre nom d’utilisateur">
-            <input v-model="registerForm.last_name" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4" type="text" placeholder="Votre nom">
-            <input v-model="registerForm.first_name" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4" type="text"
-                placeholder="Votre prénom">
+            <span class="text-xs text-red-500">{{ listError['pseudo'] }}</span>
+            <input v-model="registerForm.last_name"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
+                type="text" placeholder="Votre nom">
+            <span class="text-xs text-red-500">{{ listError['last_name'] }}</span>
+            <input v-model="registerForm.first_name"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
+                type="text" placeholder="Votre prénom">
+            <span class="text-xs text-red-500">{{ listError['first_name'] }}</span>
             <span class="self-start text-xs text-gray-500 mt-4 ">Date de naissance</span>
             <div class="flex mt-1">
                 <DateSelect v-model="registerForm.date_de_naissance" />
             </div>
-            <input v-model="registerForm.phone_number" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4" type="text"
-                placeholder="Votre numéro de téléphone">
-            <input v-model="registerForm.lieu_residence" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4" type="text"
-                placeholder="Votre lieu de residence">
-            <input v-model="registerForm.adresse_mail" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4" type="text"
-
-                placeholder="Votre adresse email">
-            <input v-model="registerForm.password" class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
-                type="password" placeholder="Votre mot de passe">
-            <input v-model="registerForm.confirmPassword"  class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4" type="password"
-                placeholder="Ressaisir votre mot de passe">
+            <input v-model="registerForm.phone_number"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
+                type="text" placeholder="Votre numéro de téléphone">
+            <span class="text-xs text-red-500">{{ listError['phone_number'] }}</span>
+            <input v-model="registerForm.lieu_residence"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
+                type="text" placeholder="Votre lieu de residence">
+            <span class="text-xs text-red-500">{{ listError['lieu_residence'] }}</span>
+            <input v-model="registerForm.adresse_mail"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
+                type="text" placeholder="Votre adresse email">
+            <span class="text-xs text-red-500">{{ listError['email'] }}</span>
+            <input v-model="registerForm.password"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
+                type="password" placeholder="Votre mot de passe">  
+                <span class="text-xs text-red-500">{{ listError['password'] }}</span>
+            <input v-model="registerForm.confirmPassword"
+                class="placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-1 text-xs mt-4"
+                type="password" placeholder="Ressaisir votre mot de passe">
+            <span class="text-xs text-red-500"> {{ listError['confirmPassword'] }}</span>
             <div class="flex justify-between w-full">
                 <div>
                     <input v-model="registerForm.acceptTerms" class="mt-4 dark:bg-white" type="checkbox">
-
-                    <span class="font-bold text-xs m-1 mt-4">J’accepte les Termes et Conditions</span>
+                    <span class="font-bold text-xs m-1 mt-4">J’accepte les Termes et Conditions</span>  
                 </div>
-
             </div>
-            <button @click="sendDataRegister"
-            :disabled="!isRegistrationValid || !registerForm.acceptTerms"
-                class="w-full p-2 rounded-md text-white mt-4 mb-6 font-bold 
+            <button @click="sendDataRegister" :disabled="isRegistrationValid && !registerForm.acceptTerms" class="w-full p-2 rounded-md text-white mt-4 mb-6 font-bold 
                      bg-[#E61B21] hover:bg-[#D21018] disabled:bg-gray-400 
-                     disabled:cursor-not-allowed">{{ props.isLoading ? 'Inscription en cours...' : 'S’inscrire' }}</button>
-                     <p v-if="!isPasswordMatch" class="text-red-500 text-xs">Les mots de passe ne correspondent pas.</p>
+                     disabled:cursor-not-allowed">{{ props.isLoading ? 'Inscription en cours...' : 'S’inscrire'
+                     }}</button>
+            <p v-if="!isPasswordMatch" class="text-red-500 text-xs">Les mots de passe ne correspondent pas.</p>
         </div>
     </div>
 </template>
 
 <script setup>
-const props = defineProps(['showAuth','isLoading'])
+const props = defineProps(['showAuth', 'isLoading', 'listError'])
 const emit = defineEmits(['connexion', 'register'])
 
 const isRegistrationValid = computed(() => {
-  return (
-    registerForm.email !== '' &&
-    registerForm.password !== '' &&
-    registerForm.phone_number !== '' &&
-    registerForm.date_de_naissance !== ''
-  );
+    return (
+        registerForm.email !== '' &&
+        registerForm.password !== '' &&
+        registerForm.phone_number !== '' &&
+        registerForm.date_de_naissance !== ''
+    );
 });
 
 const loginForm = ref({ email: '', password: '' })
-const registerForm = ref({ email: '', password: '',confirmPassword:'',first_name: '',last_name: '',phone_number:'',adresse_mail:'',date_de_naissance: '',acceptTerms: false,lieu_residence:'' })
+const registerForm = ref({ email: '', password: '', confirmPassword: '', first_name: '', last_name: '', phone_number: '', adresse_mail: '', date_de_naissance: '', acceptTerms: false, lieu_residence: '' })
 const isPasswordMatch = computed(() => {
-  return registerForm.password === registerForm.confirmPassword;
+    return registerForm.password === registerForm.confirmPassword;
 });
 
 const sendDataRegister = () => {
-  if (isRegistrationValid.value) {
-    emit('register', registerForm.value);
-  } else {
-    // Afficher un message d'erreur ou prendre une action appropriée
-    console.error('Veuillez remplir tous les champs requis avant de vous inscrire.');
-  }
+    if (isRegistrationValid.value) {
+        emit('register', registerForm.value);
+    } else {
+        // Afficher un message d'erreur ou prendre une action appropriée
+        console.error('Veuillez remplir tous les champs requis avant de vous inscrire.');
+    }
 };
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,11 @@
       </template>
     </TopNavigationBar>
     <AuthButton @login="showAuth = 'login'" @register="showAuth = 'register'" />
-    <AuthCard v-if="showAuth" :show-auth="showAuth" @connexion="onSubmit" @register="onRegister" :is-loading="isLoading" />
+    <AuthCard :listError="validate" v-if="showAuth" :show-auth="showAuth" @connexion="onSubmit" @register="onRegister"
+      :is-loading="isLoading" />
     <ProjectDescriptionCard v-else @call-to-action="showAuth = 'register'" />
   </div>
-  <Footer/>
+  <Footer />
 </template>
 
 <script setup>
@@ -21,38 +22,92 @@ const { login, createUser } = useDirectusAuth();
 
 const router = useRouter();
 
+const validate = ref([])
+
 const onSubmit = async (logindata) => {
   isLoading.value = true;
   try {
-
     await login({ email: logindata.email + '@mail.com', password: logindata.password });
     router.push('/welcome');
   } catch (e) {
     console.log(e);
-  }finally {
-    isLoading.value = false;
-  }
+  } 
 };
-
 const onRegister = async (registerdata) => {
-  isLoading.value = true;
+  const regexPhone = /^0\d{9}$/;
+  const regexName = /^[A-ZÀ-ÖØ-Ý][a-zà-öø-ý-]*(?:\s+[A-ZÀ-ÖØ-Ý][a-zà-öø-ý-]*)*$/;
+  const regexPseudo = /^[a-z]+/
+  const regexLieu = /^[A-Za-zÀ-ÖØ-Ý0-9-'\s]+$/;
+  const regexPassword = /^.{8,}$/;
   try {
+    if (!regexName.test(registerdata.first_name)) {
+      if (validate.value["first_name"] == undefined) {
+        validate.value['first_name'] = 'Prenoms invalide ou vide. Ex: Randria'
+      }
+    } else {
+      delete validate.value['first_name'] 
+    }
+    if (!regexName.test(registerdata.last_name)) {
+      if (validate.value["last_name"] == undefined) {
+        validate.value['last_name'] = 'Nom invalide ou vide. Ex: Tefy Niaina'
+      }
+    } else {
+      delete validate.value['last_name'] 
+    }
 
-    const newUser = await createUser({ 
-      email: registerdata.email + '@mail.com', 
-      password: registerdata.password, 
-      role: 'f8fc491a-643b-47fd-a929-18b4c6b35a17',
-      last_name: registerdata.last_name,
-      first_name: registerdata.first_name,
-      phone_number: registerdata.phone_number,
-      adresse_mail:registerdata.adresse_mail,
-      date_de_naissance: registerdata.date_de_naissance,
-      lieu_residence: registerdata.lieu_residence
-     });
-    showAuth.value = 'login';
+    if (!regexPseudo.test(registerdata.email)) {
+      if (validate.value["pseudo"] == undefined) {
+        validate.value['pseudo'] = 'Pseudo invalide ou vide. Ex: andy'
+      }
+    } else {
+      delete validate.value['pseudo'] 
+    }
+    if (!regexPassword.test(registerdata.password)) {
+      if (validate.value["password"] == undefined) {
+        validate.value['password'] = 'Mot de passe invalide ou vide. 8 caractères au moins'
+      }
+    } else {
+      delete validate.value['password']
+    }
+    if (!regexPhone.test(registerdata.phone_number)) {
+      if (validate.value["phone_number"] == undefined) {
+        validate.value['phone_number'] = 'Contact incorrect ou vide. Ex: 0334456789'
+      }
+    } else {
+      delete validate.value['phone_number'] 
+    }
+    if (!regexPassword.test(registerdata.confirmPassword)) {
+      if (validate.value["confirmPassword"] == undefined) {
+        validate.value['confirmPassword'] = 'Password ne correspond pas.'
+      }
+    } else {
+      if (registerdata.confirmPassword !== registerdata.password) {
+        validate.value['confirmPassword'] = 'Mot de passe invalide ou vide. 8 caractères au moins.'
+      } else {
+        delete validate.value['confirmPassword']
+      }
+    }
+    if (Object.keys(validate.value).length === 0) {
+        isLoading.value = true;
+        const newUser = await createUser({
+        email: registerdata.email+'@mail.com',
+        password: registerdata.password,
+        role: 'f8fc491a-643b-47fd-a929-18b4c6b35a17',
+        last_name: registerdata.last_name,
+        first_name: registerdata.first_name,
+        phone_number: registerdata.phone_number,
+        adresse_mail: registerdata.adresse_mail,
+        date_de_naissance: registerdata.date_de_naissance,
+        lieu_residence: registerdata.lieu_residence
+      });
+      await login({ email: registerdata.email+'@mail.com', password: registerdata.password });
+        router.push('/welcome');
+        
+    }
   } catch (e) {
-  }finally {
-    isLoading.value = false;
+    console.log(e)
+  } finally {
+    
   }
 };
 
